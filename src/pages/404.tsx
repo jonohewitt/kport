5fdda52e2394cfc45cdb625de0538b6e81e6eea4
Jono Hewitt <jonohewitt@gmail.com>
2021-09-21 22:55:52 +0200

    Clean up 404 and empty pages

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,14 +1,29 @@
 import { PageProps } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
 import React, { FC } from "react"
-import { Layout } from "../components/layout"
+import styled from "styled-components"
 import { Seo } from "../components/seo"
 
+const H1 = styled.h1`
+  text-align: center;
+  font-size: 42px;
+  font-weight: 500;
+  margin-top: 20px;
+`
+
 const NotFoundPage: FC<PageProps> = () => (
-  <Layout>
+  <>
     <Seo title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
+    <H1>404: Not Found</H1>
+    <StaticImage
+      src="../images/content/leaves.jpg"
+      alt="Light shining through horsechestnut leaves"
+      loading="eager"
+      layout="constrained"
+      imgStyle={{ objectFit: "cover" }}
+      style={{ maxWidth: "80vw", marginTop: "20px" }}
+    />
+  </>
 )
 
 export default NotFoundPage
