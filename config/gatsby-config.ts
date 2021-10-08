d299ef63f01007501ac9d0d44c03f3463397f574
Jono Hewitt <jonohewitt@gmail.com>
2021-10-08 23:40:23 +0100

    Update description

--- config/gatsby-config.ts
+++ config/gatsby-config.ts
@@ -3,7 +3,7 @@ import { GatsbyConfig } from "gatsby"
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `K:Port - Low carbon, multi-modal transport hub by Hewitt Studios LLP`,
-    description: `Proven smart city & EV charging solution, designed to inspire & enable the sustainable electrification of transport.`,
+    description: `Proven smart city & EV charging solution, designed to inspire and enable the sustainable electrification of transport.`,
     author: `Hewitt Studios LLP`,
     siteUrl: "https://kport.co.uk",
   },
