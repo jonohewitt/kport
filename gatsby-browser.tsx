e638cb63428ff2077eb932cfd4a5f07ddeb77edc
Jono Hewitt <jonohewitt@gmail.com>
2022-02-08 23:20:01 +0100

    Add types

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -1,11 +1,15 @@
-// import "@fontsource/quicksand/latin.css"
 import "remove-focus-outline"
+
+import { GatsbyBrowser } from "gatsby"
 import React from "react"
+
 import { Layout } from "./src/components/layout"
 import { GlobalProvider } from "./src/context/globalState"
 
-export const wrapRootElement = ({ element }) => (
-  <GlobalProvider>{element}</GlobalProvider>
-)
+export const wrapRootElement: GatsbyBrowser["wrapRootElement"] = ({
+  element,
+}) => <GlobalProvider>{element}</GlobalProvider>
 
-export const wrapPageElement = ({ element }) => <Layout>{element}</Layout>
+export const wrapPageElement: GatsbyBrowser["wrapPageElement"] = ({
+  element,
+}) => <Layout>{element}</Layout>
