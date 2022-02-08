4deee2f1bf7662987e347c84cc257c1e7bcd5ebb
Jono Hewitt <jonohewitt@gmail.com>
2022-02-08 23:19:47 +0100

    Dev environment updates

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,18 +8,37 @@ module.exports = {
     "eslint:recommended",
     "plugin:react/recommended",
     "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "prettier",
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ["./tsconfig.json"],
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 12,
     sourceType: "module",
   },
-  plugins: ["react", "@typescript-eslint"],
-  rules: {},
+  plugins: [
+    "react",
+    "react-hooks",
+    "jsx-a11y",
+    "@typescript-eslint",
+    "prettier",
+  ],
+  rules: {
+    "prettier/prettier": "warn",
+    "react/prop-types": "off",
+    "react/no-unescaped-entities": "off",
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "@typescript-eslint/no-unsafe-assignment": "warn",
+  },
   settings: {
+    react: {
+      version: "detect",
+    },
     linkComponents: [
       "Hyperlink",
       { name: "StyledExternalLink", linkAttribute: "href" },
